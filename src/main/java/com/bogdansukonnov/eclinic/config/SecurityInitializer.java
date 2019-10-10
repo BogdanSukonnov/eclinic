13d85b37ae3ccf2acdaa498b4d20fb059bcb81d1
Bogdan Sukonnov <bogdan.sukonnov@gmail.com>
2019-10-10 10:50:35 +0300

    Basic security config

---
+++ src/main/java/com/bogdansukonnov/eclinic/config/SecurityInitializer.java
@@ -0,0 +1,9 @@
+package com.bogdansukonnov.eclinic.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+    public SecurityInitializer() {
+    }
+}
