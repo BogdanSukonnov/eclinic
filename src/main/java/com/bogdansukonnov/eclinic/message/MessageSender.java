44cb6d5497de0dc288344fef58229e4ac09ab05d
Bogdan Sukonnov <bogdan.sukonnov@gmail.com>
2019-11-21 13:43:11 +0300

    send JMS message asynchronously ang catch exceptions

--- src/main/java/com/bogdansukonnov/eclinic/message/MessageSender.java
+++ src/main/java/com/bogdansukonnov/eclinic/message/MessageSender.java
@@ -1,6 +1,8 @@
 package com.bogdansukonnov.eclinic.message;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.jms.JmsException;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.core.MessageCreator;
 import org.springframework.lang.NonNull;
@@ -12,17 +14,31 @@ import javax.jms.TextMessage;
 
 @Component
 @AllArgsConstructor
+@Log4j2
 public class MessageSender {
 
     private JmsTemplate jmsTemplate;
 
+    /**
+     * send text message to queue asynchronously
+     *
+     * @param message text of the message
+     */
     public void sendMessage(String message) {
-        jmsTemplate.send(new MessageCreator(){
-            @Override
-            @NonNull
-            public TextMessage createMessage(@NonNull Session session) throws JMSException {
-                return session.createTextMessage(message);
-            }
-        });
+        Runnable runnable =
+                () -> {
+                    try {
+                        jmsTemplate.send(new MessageCreator() {
+                            @Override
+                            @NonNull
+                            public TextMessage createMessage(@NonNull Session session) throws JMSException {
+                                return session.createTextMessage(message);
+                            }
+                        });
+                    } catch (JmsException e) {
+                        log.error(e + " : " + e.getMessage());
+                    }
+                };
+        new Thread(runnable).start();
     }
 }
